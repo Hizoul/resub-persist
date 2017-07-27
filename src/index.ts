c630b513f2f9b587c0589db46274a9f686659886
MMB2 <git@mmb2.click>
2017-07-27 16:48:35 +0200

    :bug: Forgot to update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import matchStoreState from "./util/matchStoreState"
-import getStoreState from "./util/getStoreState"
 import persist from "./persist"
 import rehydrate from "./rehydrate"
+import getStoreState from "./util/getStoreState"
+import matchStoreState from "./util/matchStoreState"
 
 export { persist, rehydrate, matchStoreState, getStoreState }
